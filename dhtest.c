sipwise/dhtest

commit a137c6a8534e395b486d910661496f3c02b6ec2a

TT#142600 Improve signal handling
On signals that are expected to terminate the program gracefully, do not
call abort, otherwise we get core dumps for conditions, that while
unexpected are not worth a core dump, such timing out when called with
the timeout program.

Print the signal name instead of number

Change-Id: I740978882288801d71d4d3c4a1bbbde8b70e6a4f

dhtest.c
@@ -111,8 +111,13 @@ static void cleanup(void) {
 }
 
 static void sigcleanup(int sig) {
+	fprintf(stderr, "signal %s received, exiting\n", strsignal(sig));
+	cleanup();
+}
+
+static void sigabort(int sig) {
 	signal(SIGABRT, SIG_DFL);
-	fprintf(stderr, "signal %i received, aborting\n", sig);
+	fprintf(stderr, "signal %s received, aborting\n", strsignal(sig));
 	cleanup();
 	abort();
 }
@@ -130,8 +135,8 @@ int main(int argc, char *argv[])
 
 	init_rand();
 	atexit(cleanup);
-	signal(SIGSEGV, sigcleanup);
-	signal(SIGABRT, sigcleanup);
+	signal(SIGSEGV, sigabort);
+	signal(SIGABRT, sigabort);
 	signal(SIGTERM, sigcleanup);
 	signal(SIGINT, sigcleanup);
 	signal(SIGHUP, sigcleanup);

commit a335d5d972c9ebd9a2023c103cce07d790e10314

TT#142600 Exit explicitly from the cleanup signal handler
The handler needs to exit explicitly, otherwise we will continue
execution as usual. (These signals are trappable and as such will
not terminate no matter what.)

Fixes: commit a137c6a8534e395b486d910661496f3c02b6ec2a
Change-Id: If0e5ad75170ef53eb4cda18d5b20387699909577

dhtest.c
@@ -113,6 +113,7 @@ static void cleanup(void) {
 static void sigcleanup(int sig) {
 	fprintf(stderr, "signal %s received, exiting\n", strsignal(sig));
 	cleanup();
+	exit(0);
 }
 
 static void sigabort(int sig) {